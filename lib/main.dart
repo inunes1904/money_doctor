2b3b87aefc681a09003d54f6c9886c57aa45236b
Ivo Nunes <inunes1904@gmail.com>
2024-05-02 21:56:06 +0100

    Enter Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:money_doctor/bills_list_example.dart';
 import 'package:money_doctor/incomes_list_example.dart';
 
@@ -6,7 +8,92 @@ BillsListExample billsListExample = BillsListExample();
 IncomesListExample incomesListExample = IncomesListExample();
 
 void main() {
-  runApp(const MoneyDoctor());
+  runApp(MoneyDoctorApp());
+}
+
+
+class MoneyDoctorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MoneyDoctorHome(),
+        '/moneydoctor': (context) => MoneyDoctor(),
+      },
+    );
+  }
+}
+
+
+class MoneyDoctorHome extends StatelessWidget {
+  const MoneyDoctorHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MONEY DOCTOR",
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: const AssetImage("images/backgroundHome.png"),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.5),
+                BlendMode.dstATop,
+              ),
+            ),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: [
+                  const Image(
+                    image: AssetImage(
+                        "images/logo.png"
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Center(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo.shade900),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0), // Set rounded corner radius
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            // Navigate to the second page when the button is pressed
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => MoneyDoctor()),
+                            );
+                          },
+                          child: Text('ENTER'),
+                        ),
+                      ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MoneyDoctor extends StatelessWidget {
@@ -24,7 +111,7 @@ class MoneyDoctor extends StatelessWidget {
               image: const AssetImage("images/background.png"),
               fit: BoxFit.cover,
               colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.1), // Adjust opacity as needed (0.0 - fully transparent, 1.0 - fully opaque)
+              Colors.black.withOpacity(0.1),
               BlendMode.dstATop,
             ),
             ),
@@ -40,7 +127,6 @@ class MoneyDoctor extends StatelessWidget {
     );
   }
 }
-
 
 class BillsPage extends StatefulWidget {
   const BillsPage({super.key});
@@ -99,7 +185,7 @@ class _BillsPageState extends State<BillsPage> {
                   color: Colors.black45
               ),
             ),
-            SizedBox(width:40),
+            SizedBox(width:30),
             Icon(
               Icons.monetization_on_sharp,
               color: Colors.greenAccent,
@@ -125,7 +211,7 @@ class _BillsPageState extends State<BillsPage> {
                 children: billsListExample.billsRows,
               ),
             ),
-            const SizedBox(width: 88,),
+            const SizedBox(width: 73,),
             Expanded(
               child: Column(
                 children: incomesListExample.incomesRows,
